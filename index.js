2ed82f07cede5256562441ea99ee0a935caf9396
Atharva Dhumal <atharvaadhumal@gmail.com>
2025-09-14 17:01:01 +0530

    Learned about express boilerplate and dotenv

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("chai aur code");
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('atharvadhumal')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Please login at chai aur code<h1>') //we can send response in html format also and even in json
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
